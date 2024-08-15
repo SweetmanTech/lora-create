4fa38916992410eff65135ab020eea38a8ab1926
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-15 15:59:54 -0300

    I remove unused libs.

--- lib/ipfs/fetchFile.tsx
+++
@@ -1,9 +0,0 @@
-import getIpfsLink from "./getIpfsLink";
-
-const fetchFile = async (url: string) => {
-  const response = await fetch(getIpfsLink(url));
-  const blob = await response.blob();
-  return new File([blob], "filename", { type: blob.type });
-};
-
-export default fetchFile;
