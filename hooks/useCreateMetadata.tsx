9cfe2bb2a1f3e0c34c05ae8453fa47bb40b27c89
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-15 15:57:57 -0300

    I replace random strings with DRY variable names.

--- hooks/useCreateMetadata.tsx
+++ hooks/useCreateMetadata.tsx
@@ -4,16 +4,18 @@ import { useState } from 'react'
 const useCreateMetadata = () => {
   const [name, setName] = useState<string>('CC0 Music')
   const [saleStrategy, setSaleStrategy] = useState<string>('ZoraTimedSaleStrategy')
+  const imageUri = 'ipfs://bafkreic3cefqzzqhoc34e2dqqfgtctmfc7mdgcfbapizpjrlroutfzilci'
+  const animationUri = 'ipfs://bafybeicpnghwsq5xyaej4hi6vuxoi4oxplll3wfhurf2v6y6kfctrtintq'
 
   const getUri = async () =>
     await uploadJson({
       name,
       description: '',
-      image: 'ipfs://bafkreic3cefqzzqhoc34e2dqqfgtctmfc7mdgcfbapizpjrlroutfzilci',
-      animation_url: 'ipfs://bafybeicpnghwsq5xyaej4hi6vuxoi4oxplll3wfhurf2v6y6kfctrtintq',
+      image: imageUri,
+      animation_url: animationUri,
       content: {
         mime: 'audio/mpeg',
-        uri: 'ipfs://bafybeicpnghwsq5xyaej4hi6vuxoi4oxplll3wfhurf2v6y6kfctrtintq',
+        uri: animationUri,
       },
     })
 
