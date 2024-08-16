fcd3a050d9fa5b118da9c990c9bb5e46061074a2
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-16 12:09:39 -0300

    I fix links for zora drops using points metadata.

--- lib/zora/getCollectionUrl.tsx
+++ lib/zora/getCollectionUrl.tsx
@@ -1,6 +1,9 @@
 import { Address } from 'viem'
+import { baseSepolia } from 'viem/chains'
 
-const getCollectionUrl = (collectionAddress: Address) =>
-  `https://testnet.zora.co/collect/bsep:${collectionAddress}/1`
+const getCollectionUrl = (chainId: any, collectionAddress: Address) => {
+  const isTestnet = chainId == baseSepolia.id
+  return `https://${isTestnet ? 'testnet.' : ''}zora.co/collect/${isTestnet ? 'bsep' : 'base'}:${collectionAddress}/1`
+}
 
 export default getCollectionUrl
