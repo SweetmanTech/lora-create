99485c2b48afc727b01b23ebdaa261f1659d16c4
Arpit Gupta <arpitgupta1214@gmail.com>
2024-08-27 00:32:14 +0530

    rearrange components

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -11,26 +11,27 @@ import Spinner from '@/components/ui/Spinner'
 
 export default function LandingPage() {
   const { creating } = useZoraCreateProvider()
+
+  if (creating) {
+    return (
+      <>
+        <Spinner />
+        <span>Creating Post!</span>
+      </>
+    )
+  }
+
   return (
-    <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
-      {creating ? (
-        <>
-          <Spinner />
-          <span>Creating Post!</span>
-        </>
-      ) : (
-        <div className="mx-auto max-w-md text-center items-center flex flex-col gap-5">
-          <h1 className="mt-4 text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
-            Zora Create
-          </h1>
-          <MainMediaUpload />
-          <Title />
-          <SaleStrategySelect />
-          <Animation />
-          <CreateButtons />
-          <Points />
-        </div>
-      )}
+    <div className="mx-auto max-w-md text-center items-center flex flex-col gap-5">
+      <h1 className="mt-4 text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
+        Zora Create
+      </h1>
+      <MainMediaUpload />
+      <Title />
+      <SaleStrategySelect />
+      <Animation />
+      <CreateButtons />
+      <Points />
     </div>
   )
 }
