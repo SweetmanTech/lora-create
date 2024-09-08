f47c463e0c048a526d73ddfb82f76bb5828597be
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-07 21:40:27 -0300

    I remove unused props from createButtons.

--- components/CreateButton/CreateButton.tsx
+++ components/CreateButton/CreateButton.tsx
@@ -3,12 +3,12 @@
 import Button from '../Button'
 import { useZoraCreateProvider } from '@/providers/ZoraCreateProvider'
 
-const CreateButton = ({ chainId, collectionAddress, children }: any) => {
+const CreateButton = ({ children }: any) => {
   const { create } = useZoraCreateProvider()
 
   return (
     <Button
-      onClick={() => create(chainId, collectionAddress)}
+      onClick={() => create()}
       className="bg-white text-black p-3 transform hover:scale-105 transition-transform duration-150 hover:shadow-lg"
     >
       {children}
--- components/LandingPage/CreateButtons.tsx
+++ components/LandingPage/CreateButtons.tsx
@@ -1,18 +1,14 @@
-import { base, baseSepolia } from 'viem/chains'
 import CreateButton from '../CreateButton'
 import { useParams } from 'next/navigation'
 
 const CreateButtons = () => {
   const params = useParams()
-  const chainId = Number(params.chainId)
   const collection = params.collection as string
 
   return (
     <div className="flex justify-between w-full gap-2">
-      <CreateButton chainId={chainId || baseSepolia.id} collectionAddress={collection}>
-        {collection ? 'Create on Existing' : 'Preview'}
-      </CreateButton>
-      {!collection && <CreateButton chainId={chainId || base.id}>Publish</CreateButton>}
+      <CreateButton>{collection ? 'Create on Existing' : 'Preview'}</CreateButton>
+      {!collection && <CreateButton>Publish</CreateButton>}
     </div>
   )
 }
