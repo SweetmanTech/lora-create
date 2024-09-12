13d16fa452003ef7e18b594d9245bf886cedb8fc
codesage <laughingsagecoder@gmail.com>
2024-09-12 18:51:57 +0200

    handling error in getIsPro

--- lib/actions/getIsPro.ts
+++ lib/actions/getIsPro.ts
@@ -1,12 +1,17 @@
 import { Address } from "viem";
 
 async function getIsPro(address: Address) {
-  const res = await fetch(`/api/profile/is-pro?address=${address}`);
-  const data = await res.json();
-
-  if (!res.ok) throw (data ?? { message: res.statusText });
-
-  return data
+  try {
+    const res = await fetch(`/api/profile/is-pro?address=${address}`);
+    const data = await res.json();
+  
+    if (!res.ok) throw (data ?? { message: res.statusText });
+  
+    return data
+  } catch (error) {
+    console.log(error)
+    return { isPro: false }
+  }
 }
 
 export default getIsPro;
