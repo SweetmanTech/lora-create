760b88a0adaf1ae526a954384b1d0588b14b951e
codesage <laughingsagecoder@gmail.com>
2024-09-13 19:28:09 +0200

    looping through retry maxRetries in getIsPro

--- lib/getIsPro.ts
+++ lib/getIsPro.ts
@@ -1,24 +1,20 @@
 import { Address } from "viem";
 import { API_APP_URL } from "./consts";
 
-async function getIsPro(address: Address, retryCount = 0) {
-  try {
-    const res = await fetch(`${API_APP_URL}/api/profile?address=${address}`);
-    const data = await res.json();
+async function getIsPro(address: Address, maxRetries = 1) {
+  for (let i = 0; i < maxRetries; i++) {
+    try {
+      const res = await fetch(`${API_APP_URL}/api/profile?address=${address}`);
+      const data = await res.json();
 
-    if (!res.ok) throw (data ?? { message: res.statusText });
+      if (!res.ok) throw (data ?? { message: res.statusText });
 
-    const result = data as { isPro: boolean };
-
-    if (!result.isPro && retryCount < 1) {
-      return getIsPro(address, retryCount + 1);
+      if (data?.isPro) return data as { isPro: boolean };
+    } catch (e) {
+      console.error(e);
     }
-
-    return result;
-  } catch (e) {
-    console.error(e);
-    return { isPro: false };
   }
+  return { isPro: false };
 }
 
 export default getIsPro;
