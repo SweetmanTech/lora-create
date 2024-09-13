bb742a5e782690d212acfa54bdcba2ed6ef0b49d
codesage <laughingsagecoder@gmail.com>
2024-09-13 19:59:39 +0200

    adding same origin middleware on api/ipfs/generate-jwt

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,15 @@
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export function middleware(request: Request) {
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-url', request.url)
+export function middleware(request: NextRequest) {
+  const origin = request.headers.get('origin')
 
-  return NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  })
+  if (origin !== process.env.NEXT_PUBLIC_APP_URL) {
+    return NextResponse.json({ message: "Unauthorized origin" }, { status: 403 })
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/api/ipfs/generate-jwt',
 }
