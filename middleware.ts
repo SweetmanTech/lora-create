ac6967af9c9283147fd37e7067e409c67b8a2360
codesage <laughingsagecoder@gmail.com>
2024-09-16 16:18:49 +0200

    updating middleware same origin validation

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,18 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  const origin = request.headers.get('origin')
-
-  if (origin !== process.env.NEXT_PUBLIC_APP_URL) {
-    return NextResponse.json({ message: "Unauthorized origin" }, { status: 403 })
+export function middleware(req: NextRequest) {
+  const referer = req.headers.get("referer");
+  const origin = req.headers.get("origin");
+  const host = req.headers.get("host");
+  if (
+    (referer && referer.includes(host)) ||
+    (origin && origin.includes(host))
+  ) {
+    return NextResponse.next()
   }
-  return NextResponse.next()
+
+  return NextResponse.json({ message: "Unauthorized origin" }, { status: 403 })
 }
 
 export const config = {
