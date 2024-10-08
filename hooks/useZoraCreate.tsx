7087c81d94f2d8dd6fb4b3d21532a45d9fa3302d
SeniorDev <tech.eng.me@gmail.com>
2024-10-08 13:28:38 -0400

    I fix feedback.

--- hooks/useZoraCreate.tsx
+++ hooks/useZoraCreate.tsx
@@ -1,29 +1,40 @@
 'use client'
 
 import { useState } from 'react'
-import { useAccount, useSwitchChain } from 'wagmi'
+import { useAccount, useSwitchChain, useWriteContract } from 'wagmi'
 import { useWriteContracts } from 'wagmi/experimental'
 import { PROFILE_APP_URL } from '@/lib/consts'
 import { usePaymasterProvider } from '@/providers/PaymasterProvider'
 import useCreateSuccess from '@/hooks/useCreateSuccess'
-import { useParams, useRouter } from 'next/navigation'
-import { Address } from 'viem'
+import { useParams, useRouter, useSearchParams } from 'next/navigation'
+import { Address, isAddress } from 'viem'
 import useZoraCreateParameters from './useZoraCreateParameters'
 import handleTxError from '@/lib/handleTxError'
+import { useProfileProvider } from '@/providers/ProfileProvider'
 
 export default function useZoraCreate() {
   const { push } = useRouter()
   const { address } = useAccount()
   const { getCapabilities } = usePaymasterProvider()
+  const { profile } = useProfileProvider()
   const { data: callsStatusId, writeContractsAsync } = useWriteContracts()
   const { switchChainAsync } = useSwitchChain()
   const [creating, setCreating] = useState<boolean>(false)
   const params = useParams()
+  const searchParams = useSearchParams()
+  const defaultAdmin = searchParams.get('defaultAdmin')
+  const connnectedProfileAddress = profile?.connectedZoraProfile?.address
+  const fallbackCreatorAddress = isAddress(defaultAdmin) ? defaultAdmin : address
+  const creatorAddress = connnectedProfileAddress || fallbackCreatorAddress
 
   const collection = params.collection as Address | undefined
   const { fetchParameters, createMetadata } = useZoraCreateParameters(collection)
 
-  useCreateSuccess(callsStatusId, () => push(`${PROFILE_APP_URL}/${address}`), !!params.collection)
+  useCreateSuccess(
+    callsStatusId,
+    () => push(`${PROFILE_APP_URL}/${creatorAddress}`),
+    !!params.collection,
+  )
 
   const create = async (chainId) => {
     setCreating(true)
